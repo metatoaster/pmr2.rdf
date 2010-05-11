103319e2dc698c32b5f5d5e82d214e4d92c84390
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2010-05-11 03:03:04 +0000

    Added method for querying basic values
      - Added a basic dublin core value retrieval method based on this into
        a new subclass for basic/generic metadata handling for PMR2.
      - Added tests.
    
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.rdf/trunk@4928 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/rdf/base.py
+++ pmr2/rdf/base.py
@@ -1,17 +1,18 @@
 from cStringIO import StringIO
 from lxml import etree
+import rdflib
 
 from pmr2.rdf.graph import PMR2Graph
-
-namespaces = {
-    'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-}
 
 
 class RdfXmlObject(object):
     """
     RDF/XML helper class.
     """
+
+    _base_namespaces = {
+        'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    }
 
     def __init__(self):
         self._purge()
@@ -20,6 +21,10 @@ class RdfXmlObject(object):
         self.graph = None
         self.dom = None
         self.subgraphIds = []
+
+    @property
+    def namespaces(self):
+        return self._base_namespaces
 
     def parse(self, input):
         """\
@@ -41,10 +46,101 @@ class RdfXmlObject(object):
             '/rdf:RDF',
         ]
         for expr in xpath_expr:
-            rdfnodes.extend(self.dom.xpath(expr, namespaces=namespaces))
+            rdfnodes.extend(self.dom.xpath(expr, namespaces=self.namespaces))
 
         for node in rdfnodes:
             s = StringIO(etree.tostring(node))
             s.seek(0)
             subgraph = self.graph.parse(s)
             self.subgraphIds.append((node.sourceline, subgraph.identifier))
+
+    def mergeNs(self, otherNs):
+        ns = {}
+        ns.update(self.namespaces)
+        ns.update(otherNs)
+        return ns
+
+    def query(self, q, initBindings={}, otherNs={}):
+        ns = self.mergeNs(otherNs)
+        return self.graph.query(q, initBindings=initBindings, initNs=ns)
+
+    def queryKeys(self, node, keys, opt_keys=[], otherNs={}):
+        """\
+        This construct keys/value pair and dynamic simple query.
+        """
+
+        # XXX I want to be able to let all values be potentially 
+        # optional, but doesn't seem like SPARQL is meant to do that.
+
+        if isinstance(node, basestring):
+            node = rdflib.URIRef(node)
+
+        bindings = {
+            rdflib.Variable('?node'): node,
+        }
+
+        base_q = 'SELECT %s WHERE \n{\n%s}'
+        base_stmt = '    ?node %s %s .\n'
+        opt_stmt  = '    OPTIONAL { ?node %s %s . }\n'
+        frag_key = []
+        frag_stmt = []
+        for k in keys:
+            ns, elem = k.split(':')
+            q_key = '?' + elem
+            frag_key.append(q_key)
+            if k in opt_keys:
+                frag_stmt.append(opt_stmt % (k, q_key))
+            else:
+                frag_stmt.append(base_stmt % (k, q_key))
+
+        q = base_q % (' '.join(frag_key), ''.join(frag_stmt))
+
+        ns = self.mergeNs(otherNs)
+        self._lastq = q
+        return self.graph.query(q, initBindings=bindings, initNs=ns)
+
+
+class RdfXmlMetadata(RdfXmlObject):
+    """\
+    Some sample methods for queries.
+    """
+
+    _base_namespaces = {
+        'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+        'pmr2': 'http://namespace.physiomeproject.org/pmr2#',
+        'pmr2note': 'http://namespace.physiomeproject.org/pmr2/note#',
+        'dc': 'http://purl.org/dc/elements/1.1/',
+        'dcterms': 'http://purl.org/dc/terms/',
+        'vCard': 'http://www.w3.org/2001/vcard-rdf/3.0#',
+    }
+
+    def queryEFNote(self, name):
+        """\
+        A rough way to get data that may be used to populate an exposure
+        file note.
+        """
+
+        subject = self.namespaces['pmr2note'] + name
+        bindings = {
+            rdflib.Variable('?type'): rdflib.URIRef(subject),
+        }
+        q = """\
+        SELECT ?key ?value WHERE {
+            ?node pmr2:type ?type .
+            ?node pmr2:fields [ ?li ?fnode ] .
+            ?fnode pmr2:field [ pmr2:key ?key ] .
+            ?fnode pmr2:field [ pmr2:value ?value ] .
+        }
+        """
+        results = self.query(q, bindings, self.namespaces)
+        return [(i[0].strip(), i[1].strip()) for i in results]
+
+    def queryDC(self, subject):
+        """\
+        Returns some Dublin Core information from the target node.
+        """
+
+        keys = ['dc:title', 'dc:creator', 'dc:description']
+        opt_keys = ['dc:creator', 'dc:description']
+        result = self.queryKeys(subject, keys, opt_keys, self.namespaces)
+        return [tuple(zip(keys, [j and j.strip() for j in i])) for i in result]
--- pmr2/rdf/tests/test_base.py
+++ pmr2/rdf/tests/test_base.py
@@ -6,6 +6,7 @@ from cStringIO import StringIO
 import rdflib
 
 from pmr2.rdf.base import RdfXmlObject
+from pmr2.rdf.base import RdfXmlMetadata
 
 testroot = dirname(__file__)
 input_dir = join(testroot, 'input')
@@ -34,9 +35,60 @@ class BaseTestCase(unittest.TestCase):
         self.parse(join(input_dir, 'simple.rdf'))
         self.assertEqual(len(self.file.subgraphIds), 1)
 
+
+class MetadataTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.file = RdfXmlMetadata()
+
+    def tearDown(self):
+        pass
+
+    def parse(self, filename):
+        fd = open(filename)
+        try:
+            self.file.parse(fd)
+        finally:
+            fd.close()
+
+    def test_0000_queryEFNote(self):
+        self.parse(join(input_dir, 'well-formed-3-node.xml'))
+        answer = {
+            u'key1': u'value1',
+            u'key2': u'value2',
+        }
+        # must convert to dictionary for testign because of multiple 
+        # results being unordered.
+        results = dict(self.file.queryEFNote('ex'))
+        self.assertEqual(answer, results)
+
+    def test_0100_queryDC(self):
+        self.parse(join(input_dir, 'well-formed-3-node.xml'))
+        answer = [(
+            ('dc:title', u'Test XML 3 node'),
+            ('dc:creator', u'Tommy'), 
+            ('dc:description', u'A test RDF/XML document'),
+        )]
+        # single result set, query ordered.
+        results = self.file.queryDC('')
+        self.assertEqual(answer, results)
+
+    def test_0101_queryDC_partial(self):
+        self.parse(join(input_dir, 'partial_dc.rdf'))
+        answer = [(
+            ('dc:title', u'Test RDF File'),
+            ('dc:creator', u'Example'), 
+            ('dc:description', None),
+        )]
+        # single result set, query ordered.
+        results = self.file.queryDC('')
+        self.assertEqual(answer, results)
+
+
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(BaseTestCase))
+    suite.addTest(unittest.makeSuite(MetadataTestCase))
     return suite
 
 if __name__ == '__main__':
