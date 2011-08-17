82de979a215400ca2e725a465563ba22e3c8185c
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2011-08-17 03:25:53 +0000

    Applying the useful methods from rdfstore branch.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.rdf/trunk@5975 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/rdf/graph.py
+++ pmr2/rdf/graph.py
@@ -1,6 +1,11 @@
 try:
-    from rdflib.graph import ConjunctiveGraph
+    # rdflib 3
+    from rdflib.graph import ConjunctiveGraph, Graph
     PMR2Graph = ConjunctiveGraph
+    rdflib.plugin.register('sparql', rdflib.query.Processor,
+                           'rdfextras.sparql.processor', 'Processor')
+    rdflib.plugin.register('sparql', rdflib.query.Result,
+                           'rdfextras.sparql.query', 'SPARQLQueryResult')
 except ImportError:
     from rdflib.Graph import ConjunctiveGraph, Graph
 
@@ -17,3 +22,33 @@ except ImportError:
             context.remove((None, None, None))
             context.parse(source, publicID=publicID, format=format, **args)
             return context
+
+from cStringIO import StringIO
+from lxml import etree
+import rdflib
+
+namespaces = {
+    'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+}
+
+def parseXML(source, publicID=None, context=None, **args):
+
+    if not hasattr(source, 'read'):
+        source = StringIO(source)
+
+    if context is None:
+        context = Graph()
+
+    rdfnodes = []
+    xpath_expr = [
+        './/rdf:RDF',
+        '/rdf:RDF',
+    ]
+    dom = etree.parse(source)
+    for expr in xpath_expr:
+        rdfnodes.extend(dom.xpath(expr, namespaces=namespaces))
+    for node in rdfnodes:
+        s = StringIO(etree.tostring(node))
+        s.seek(0)
+        context.parse(s, publicID=publicID, format='xml', **args)
+    return context
